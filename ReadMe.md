838d35467435ad5da85dbf61b6718d97ddf62374
Lvv.me <cntrump@gmail.com>
2022-12-15 16:39:10 +0800

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Default Xcode theme
+
+Changed font size to 11pt.
+
+```bash
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -exu
+
+cp -f *.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
